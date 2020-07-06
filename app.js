792c9d9c80399e2cef2c1d2acf2ed2d620170b3a
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-07-06 15:15:57 -0600

    class setup

---
+++ app.js
@@ -0,0 +1,34 @@
+
+class Cat {
+  constructor(name, color, gender, weight, sound, vegetarian, species) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.sound = sound;
+    this.vegetarian = vegetarian;
+    this.species = species;
+  }
+}
+
+let calico = new Cat("Koda", "Orange Black", "F", 10, "Meow", "Yes", "House Cat")
+let mountainLion = new Cat("Big Boy", "Brown", "F", 75, "Roar", "No", "Lion")
+let puma = new Cat("Fast Cat", "Black", "F", 50, "Roar", "No", "Puma")
+let tabbyCat = new Cat("Jax", "Orange", "M", 10, "Meow", "Yes", "House Cat")
+class Monkey {
+  constructor(name, color, gender, weight, sound, vegetarian, species) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.sound = sound;
+    this.vegetarian = vegetarian;
+    this.species = species;
+  }
+}
+
+
+
+
+
+
